fedada5327ce386718cd816425875692f890a0f3
Dymika Harte <dymika@unsgnd.ca>
2021-11-04 03:33:08 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const temperature = -2
+const temperature = -41
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -10,3 +10,19 @@ console.log("Short sleeves won't cut it!");
 
   console.log("Now you're ready to go outside!");
 
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.")
+};
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...")
+};
+
+const raining = false
+if(!raining) {
+  console.log("Leave your umbrella at home!")
+};
